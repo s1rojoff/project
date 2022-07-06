f4bdfc9d1a91ca6aedf10b00e58bcce30ee7082e
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2022-07-06 14:46:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-red-500">saom</h1>
+    <p class="text-green-600 te">niam</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
