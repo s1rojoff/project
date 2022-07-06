bac37101e48ef1720b323545f9c175515288fe52
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2022-07-06 18:23:47 +0300

    first section created

---
+++ src/components/Main/VueMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <FirstSection/>
+  </div>
+</template>
+
+<script>
+import FirstSection from './FirstSection.vue';
+export default {
+    name: "VueMain",
+    components: { FirstSection }
+}
+</script>
+
+<style>
+
+</style>
