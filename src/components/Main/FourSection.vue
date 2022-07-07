5a5e7d872fca74d228655b0ab5459a8abca26183
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2022-07-07 15:40:14 +0300

    some changes

--- src/components/Main/FourSection.vue
+++ src/components/Main/FourSection.vue
@@ -1,17 +1,17 @@
 <template>
-  <section class="bg-orange-50 mt-10">
+  <section class="bg-orange-50 h-4/5 mt-10">
     <div class="grid grid-cols-2 gap-10 mt-10 ml-10 py-0 px-10">
       <div class="h-auto ">
         <img
           src="./../../assets/img/peace@2x_158f4453627629ae1dd3ec115a559630.webp"
           alt=""
-          class="w-4/5 h-full -top-7 relative ml-10"
+          class="w-2/3 h-full -top-11 relative ml-10"
         />
       </div>
 
       <div class="flex items-center justify-center flex-col">
         <div>
-          <h1 class="text-5xl font-bold">Achieve peace of mind with Todoist</h1>
+          <h1 class="text-4xl font-bold">Achieve peace of mind with Todoist</h1>
           <StartBtn />
         </div>
       </div>
