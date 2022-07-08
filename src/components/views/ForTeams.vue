a34aada5dceceacc4c710db35ea3fb61d56ae1c3
Javohir <javohirjamolovich061@gmail.com>
2022-07-08 16:29:53 +0500

    For teams images

--- src/components/views/ForTeams.vue
+++ src/components/views/ForTeams.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    
+    <ForTeamsText/>
+    <TeamImg/>
+    <SecTeamImg/>
   </div>
 </template>
 
 <script>
-
+import ForTeamsText from "../ForTeamsViews/ForTeamsText.vue"
+import TeamImg from "../ForTeamsViews/TeamImg.vue"
+import SecTeamImg from "../ForTeamsViews/SecTeamImg.vue"
 export default {
-    name: "ForTeams"
+    name: "ForTeams",
+    components: {ForTeamsText, TeamImg, SecTeamImg}
 }
 </script>
 
