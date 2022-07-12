4412ffdef02aa5ada6e8f9e834f37e4a457ef21f
shukhratov1ch <jorayevazik586@gmail.com>
2022-07-12 11:48:09 +0300

    install img

--- src/components/views/Templates.vue
+++ src/components/views/Templates.vue
@@ -2,15 +2,17 @@
   <div>
     <TempHeaderText/>
     <TempFirstSec/>
+    <category-sec/>
   </div>
 </template>
 
 <script>
 import TempHeaderText from '../templatesViews/TempHeaderText.vue';
 import TempFirstSec from '../templatesViews/TempFirstSec.vue';
+import CategorySec from '../templatesViews/CategorySec.vue';
 export default {
     name: "Templates",
-    components: { TempHeaderText, TempFirstSec }
+    components: { TempHeaderText, TempFirstSec , CategorySec }
 }
 </script>
 
